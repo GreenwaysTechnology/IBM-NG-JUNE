be17ef949774e963e45bd12892fb9f39c651e82d
Subramanian Murugan <sasubramanian_md@hotmail.com>
2020-06-10 14:50:32 +0530

    structural directives

--- ng-databinding/src/app/app.component.ts
+++ ng-databinding/src/app/app.component.ts
@@ -12,10 +12,37 @@ export class AppComponent{
 
     //List
     numList:Array<number> = [1,2,3,4,5,6,7,8];
+
+    //progress bar variables
+    loading:boolean = true;
+    status:string = 'Loading......'
+
+    constructor(){
+      //Progress bar time: using timer
+      setTimeout(()=>{
+        this.loading = !this.loading;
+        this.status = 'Finished'
+      },5000)
+
+    }
     
     //User List
     users:Array<any> = USERS;
+    //ngIf
+    isHidden:boolean =false;
+    isEnabled:boolean = false;
 
+    //switch..case
+    feedback:string = 'good';
+
+    toggle(){
+       this.isHidden  = !this.isHidden;
+    }
+   
+    toogleEnabled(){
+        this.isEnabled = !this.isEnabled;
+
+    }
 
 
 }
