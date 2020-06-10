5c74bf0f1397d254919363495efd3229e954ac51
Subramanian Murugan <sasubramanian_md@hotmail.com>
2020-06-10 17:04:39 +0530

    pipes

--- ng-databinding/src/app/app.component.ts
+++ ng-databinding/src/app/app.component.ts
@@ -3,14 +3,37 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'app-root',
+    selector :'app-root',
     templateUrl: 'app.component.html'
 })
 export class AppComponent{
-    color:string = 'red'
-    firstname:string ='Default'
 
-    public setName(name:string){
-       this.firstname = name;
-    }
+    //date data
+    today:any = new Date();
+    someDay:any = new Date(1999,6,10);
+
+    //currency data
+    stockValue:number =100.7863;
+
+    //strings
+    name:string = 'subramanian';
+
+    //User object
+    user:any= {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        }
+    };
+
 }
--- ng-databinding/src/app/app.module.ts
+++ ng-databinding/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { ProfileComponent } from './profile.component';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HighLightDirective} from './directives/app.highlight.directive';
+import {DecimalFractionPipe} from './pipes/app.decimalfraction';
 
 
 
 @NgModule({
   //di list: ng wil lookup this list , and create objects and link accordingly
   //declarations:[AppComponent,ProfileComponent,ParentComponent,ChildComponent],
-  declarations : [AppComponent,HighLightDirective],
+  declarations : [AppComponent,HighLightDirective,DecimalFractionPipe],
   imports : [BrowserModule ,FormsModule],
   providers: [],
   ///entry point
