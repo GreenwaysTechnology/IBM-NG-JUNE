296e4a3c89e2d103a2d75790be50c2fba0409435
Subramanian Murugan <sasubramanian_md@hotmail.com>
2020-06-10 12:35:57 +0530

    custom events

--- ng-databinding/src/app/app.component.ts
+++ ng-databinding/src/app/app.component.ts
@@ -1,4 +1,52 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output } from '@angular/core';
+
+
+
+@Component({
+    selector: "app-parent",
+    template :`
+        <div>
+            <h1>Parent Component</h1>
+            <hr>
+            <h2>Messsage From Child : {{message}}</h2>
+            <app-child (onSendRequest)="onUpdate($event)" ></app-child>
+        </div>
+     `
+})
+export class ParentComponent{
+
+    message : string;
+
+    onUpdate(e:any){
+       //alert('Parent Message' + e);
+       this.message = e;
+    }
+}
+
+@Component({
+    selector: "app-child",
+    template: ` 
+       <h1>Child Component</h1>
+    <button class="btn btn-success" (click)="sendData()">Send Data to Parent</button>
+ `
+
+})
+export class ChildComponent{
+
+    //message to be sent to parent
+    message:string = "Hello I am from Child!";
+
+    //custom event: user defined event
+    @Output()
+    onSendRequest = new EventEmitter<string>();
+
+    sendData(){
+     alert('Sending data to Parent');
+      this.onSendRequest.emit(this.message)
+    }
+
+}
+
 
 
 
--- ng-databinding/src/app/app.module.ts
+++ ng-databinding/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { AppComponent ,ParentComponent,ChildComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProfileComponent } from './profile.component';
 import {FormsModule} from '@angular/forms';
@@ -8,7 +8,7 @@ import {FormsModule} from '@angular/forms';
 
 @NgModule({
   //di list: ng wil lookup this list , and create objects and link accordingly
-  declarations:[AppComponent,ProfileComponent],
+  declarations:[AppComponent,ProfileComponent,ParentComponent,ChildComponent],
   imports : [BrowserModule ,FormsModule],
   providers: [],
   ///entry point
